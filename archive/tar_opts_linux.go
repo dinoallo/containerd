b578afd7987ebd2abba807327d7ae2f63058f320
Yun Pan <dinoallo@netc.it>
2025-08-28 18:51:54 +0800

    Fix handling of existing originalPath in OverlayConvertWhiteout
    Signed-off-by: Yun Pan <dinoallo@netc.it>

--- archive/tar_opts_linux.go
+++ archive/tar_opts_linux.go
@@ -46,6 +46,14 @@ func OverlayConvertWhiteout(hdr *tar.Header, path string) (bool, error) {
 		originalBase := base[len(whiteoutPrefix):]
 		originalPath := filepath.Join(dir, originalBase)
 
+		// Remove originalPath if it exists
+		if _, err := os.Stat(originalPath); err == nil {
+			if err := os.Remove(originalPath); err != nil {
+				return false, err
+			}
+		} else if !os.IsNotExist(err) {
+			return false, err
+		}
 		if err := unix.Mknod(originalPath, unix.S_IFCHR, 0); err != nil {
 			return false, err
 		}
